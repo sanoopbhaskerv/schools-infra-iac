e951b4d59cd938a8c34872cc69cb5fcd1fd63361
Sanoop Bhasker V <sanoopbhaskerv@Sanoops-MacBook-Pro.local>
2025-12-05 23:05:06 +0530

    Fixing Recurring RDS Modification

--- rds.tf
+++ rds.tf
@@ -42,6 +42,7 @@ resource "aws_db_instance" "main" {
   allocated_storage = 20
   storage_type      = "gp3"
   backup_retention_period = 0 # Disable backups to save cost
+  apply_immediately = true
 
   db_name  = var.db_name
   username = var.db_username
