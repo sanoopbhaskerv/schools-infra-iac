6287bb6ecdeb87350b85d7318162f705f4244332
Sanoop Bhasker V <sanoopbhaskerv@Sanoops-MacBook-Pro.local>
2025-12-05 22:31:07 +0530

    DB cost optimization

--- rds.tf
+++ rds.tf
@@ -40,7 +40,8 @@ resource "aws_db_instance" "main" {
   engine_version    = "14"
   instance_class    = "db.t3.micro"
   allocated_storage = 20
-  storage_type      = "gp2"
+  storage_type      = "gp3"
+  backup_retention_period = 0 # Disable backups to save cost
 
   db_name  = var.db_name
   username = var.db_username
