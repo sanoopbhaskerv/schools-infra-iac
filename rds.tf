7b66a5c8d8accd35a1b59586c6d22b0e9f5d3e36
Sanoop Bhasker V <sanoopbhaskerv@Sanoops-MacBook-Pro.local>
2025-12-06 15:56:36 +0530

    Allow public access to RDS for debugging

--- rds.tf
+++ rds.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "rds" {
     security_groups = [aws_security_group.ecs_tasks.id]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 5432
+    to_port     = 5432
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
